ce44c740fa60e5b309babb040a00bee952c15dad
Mkkarwani <147414429+Mkkarwani@users.noreply.github.com>
2024-12-23 17:50:48 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'studyfreinds-cache-v1';
+const OFFLINE_URL = '/offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([OFFLINE_URL]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+  );
+});
